617c84a3eb2e9c5032647579145ad75e83a09b88
Andreas Nilsson <andreas.nilsson@jayway.com>
2022-03-12 19:37:05 +0100

    Repl instructions.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,12 @@ using namespace std;
 int main() {
   snabl::M m;
   m.abc_lib.emplace(m);
-  cout << "Snabl v1" << endl << endl;
+
+  cout <<
+    "Snabl v1" << endl << endl <<
+    "Hit Enter twice to evaluate," << endl <<
+    "Ctrl+D quits." << endl << endl;
+  
   snabl::repl(cin, cout, m);
   return 0;
 }
