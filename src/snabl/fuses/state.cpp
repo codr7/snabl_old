667ee021ce8bd42aa77aa34d92fba78f90004aa7
Andreas Nilsson <andreas.nilsson@jayway.com>
2022-04-26 04:57:48 +0200

    Improves state fuse.

--- src/snabl/fuses/state.cpp
+++ src/snabl/fuses/state.cpp
@@ -8,35 +8,35 @@ namespace snabl::fuses {
     int n = 0;
 
     for (PC pc = fun->start_pc; pc < m.emit_pc;) {
-      Op &op1 = m.ops[pc];
+      PC pc1 = pc;
+      Op &op1 = m.ops[pc1];
 
       if (op_code(op1) == OpCode::STATE_BEG) {
 	vector<PC> pcs;
 	int count = ops::state_beg_count(op1);
-
+	pc = ops::state_beg_next(op1);
+	
 	for (;;) {
-	  pc = drill_pc(pc+1, m).first;
+	  pc = drill_pc(pc, m).first;
 	  Op op2 = m.ops[pc];
 	  
 	  if (op_code(op2) == OpCode::STATE_BEG) {
 	    pcs.push_back(pc);
 	    count += ops::state_beg_count(op2);
+	    pc = ops::state_beg_next(op2);
 	  } else {
 	    break;
 	  }
 	}
 
-	if (!pcs.empty()) {
-	  ops::STATE_BEG(op1, ops::state_beg_next(m.ops[pcs.back()]), count);	  
-
-	  for (PC pc: pcs) {
-	    cout << "Fusing " << fun << " STATE: ";
-	    op_trace(pc, cout, m);
-	    Op &op = m.ops[pc];
-	    ops::NOP(op);
-	    n++;
-	  }
+	if (pc != ops::state_beg_next(op1) || count != ops::state_beg_count(op1)) {
+	  ops::STATE_BEG(op1, pc, count);
+	  cout << "Fusing " << fun << " STATE: ";
+	  op_trace(pc, cout, m);
+	  n++;
 	}
+
+	for (PC pc: pcs) { ops::NOP(m.ops[pc]); }
       }
 
       pc += op_len(m.ops[pc]);
